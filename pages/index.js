f044b6be755f6df1f1cf36f8149f50babdc2a4c4
Gabriel Mizael <gabriel-apmizael@hotmail.com>
2024-12-04 20:44:02 +0000

    Alterando mensagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Quando se olha muito tempo para um abismo, o abismo olha para você.</h1>
+    return <h1>Quando se olha muito tempo para um abismo, o abismo olha para você!</h1>
 }
 
 export default Home; 
